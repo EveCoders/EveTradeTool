b6e4c8d39d131399dc8401bc68d966e168778530
David Scheiber-Jakob <dscheiberjakob@me.com>
2014-07-08 01:01:35 +0000

    added code to perform a simple marketstats query and save as an xml object.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+/*Main JS, at your service*/
 
+$(document).ready(function(){
+    
+    $('#fetch-marketstats').on('click', function(){
+        
+        var $xmlMarketData; /*Ein jQuery Objekt mit einem XML drin.*/
+        
+        var url = 'http://api.eve-central.com/api/marketstat',
+            settings = {
+               typeid : 34,
+               hours : 1,
+               regionlimit : 10000002
+            };
+    
+        $.get(url, settings).done(function(data){
+            $xmlMarketData = $(data);
+            console.log("loaded Marketdata.");
+        });
+        
+    });
+    
+});
+    
+
+    
+    
