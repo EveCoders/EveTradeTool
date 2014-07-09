554560027a8652a76848d90cd8eaa00423fe27a5
David Scheiber-Jakob <dscheiberjakob@me.com>
2014-07-09 02:54:27 +0000

    implemented simple item search.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 /*Main JS, at your service*/
 
+var item_name_string;
 $(document).ready(function(){
     
     var $tableBody = $('#market_table');
     var $xmlMarketData; /*Ein jQuery Objekt mit einem XML drin.*/
-    
     $('#fetch-marketstats').on('click', function(){
-        
+        // Get item name form value string
+        item_name_string = $('#input-item_name').val();
+        // In case we didn't have text input substitute by a default search string
+        if (item_name_string === '') item_name_string = 'Tritanium';
+        console.log("Item Name: " + item_name_string);
+
+        // lookup corresponding typeID of first matching item
+        //var matched_items = searchForItemWithName(item_name_string);
+        //console.log("Matched items: " + matched_items);
+        var tid = getFirstMatchingTypeID(item_name_string);
+        if (tid === -1) {
+            alert('Sorry, no item found.');
+            return; // bypass market data query and return early
+        }
+
         var url = 'http://api.eve-central.com/api/quicklook',
             settings = {
-               typeid : 34,
+               typeid : tid,
                sethours : 1,
                regionlimit : 10000002
             };
