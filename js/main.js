e229dd59c0cb44d02d2d2dab263e9780b617a76e
David Scheiber-Jakob <dscheiberjakob@me.com>
2014-07-11 02:31:44 +0000

    added autosuggestion functionality for regions.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 
 
 $(document).ready(function(){
-    
+  
+  /*activate plugins*/
+  
+  /*----------------*/
+  
+  
+
     var $tableBody = $('#market_table');
     var $xmlMarketData; /*Ein jQuery Objekt mit einem XML drin.*/
-    var item_name_string;
+    var item_name_string, region_name_string;
     
     // autosuggestions while typing
     $('#input-item_name').on('keyup', function() {
@@ -23,7 +29,6 @@ $(document).ready(function(){
         
         $(matched_items).each(function(){
           $('#input-item-guess-field').append('<li class="list-group-item">'+this.typeName+'</li>');
-          
         })
         
         $('#input-item-guess-field').contents().on('click', function(){
@@ -32,8 +37,33 @@ $(document).ready(function(){
           });
         
     })
+    
+    $('#input-region_name').on('keyup', function() {
+        region_name_string = $('#input-region_name').val();
+        // skip rest if we don't have at least two characters
+        if ( region_name_string.length < 2) {
+          $('#input-region-guess-field').parent().fadeOut();
+          return;
+          
+        }
+        var matched_regions = searchForRegionWithName(region_name_string);
+        
+        $('#input-region-guess-field').empty();
+        $('#input-region-guess-field').parent().fadeIn();
+        
+        $(matched_regions).each(function(){
+          $('#input-region-guess-field').append('<li class="list-group-item">'+this.regionName+'</li>');
+        })
+        
+        $('#input-region-guess-field').contents().on('click', function(){
+           $('#input-region_name').val($(this).text());
+           $('#input-region-guess-field').parent().fadeOut();
+          });
+    })
+    
 
     $('#fetch-marketstats').on('click', function(){
+        /* ITEM text field */
         // Get item name form value string
         item_name_string = $('#input-item_name').val();
         // In case we didn't have text input substitute by a default search string
@@ -45,15 +75,31 @@ $(document).ready(function(){
         //console.log("Matched items: " + matched_items);
         var tid = getFirstMatchingTypeID(item_name_string);
         if (tid === -1) {
-            alert('Sorry, no item found.');
+            alert('Sorry, no matching item found.');
             return; // bypass market data query and return early
         }
 
+        /* REGION text field */
+        // Get region name form value string
+        region_name_string = $('#input-region_name').val();
+        // In case we didn't have text input substitute by a default search string
+        if (region_name_string === '') region_name_string = 'Domain';
+        console.log("Region Name: " + region_name_string);
+
+        // lookup corresponding typeID of first matching item
+
+        //console.log("Matched items: " + matched_items);
+        var rid = getFirstMatchingRegionID(region_name_string);
+        if (rid === -1) {
+            alert('Sorry, no matching region found.');
+            return; // bypass market data query and return early
+        }
+        
         var url = 'http://api.eve-central.com/api/quicklook',
             settings = {
                typeid : tid,
-               sethours : 1,
-               regionlimit : 10000002
+               sethours : 24,
+               regionlimit : rid
             };
     
         $.get(url, settings).done(function(data){
